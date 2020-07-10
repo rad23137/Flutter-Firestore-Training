939f816cd8d7742834f7261822643fff0821053b
Radhika <radhika23137@gmail.com>
2020-07-10 15:42:45 +0530

    Long List Implementation

--- FlutterHandsOn/explore_more_widgets/lib/main.dart
+++ FlutterHandsOn/explore_more_widgets/lib/main.dart
@@ -4,15 +4,41 @@ void main() {
   runApp(MaterialApp(
     title: "Exploring ListView",
     home: Scaffold(
-        appBar: AppBar(title: Text("Basic List View")),
+        appBar: AppBar(title: Text("Long List")),
         body: getListView() // called it in scaffold widget because
-                            // the scrollable content can overflow screen
-                           // so would through exception on direct calling so wrapped inside scaffold widget
+        // the scrollable content can overflow screen
+        // so would through exception on direct calling so wrapped inside scaffold widget
         ),
   ));
 }
 
+// function to generate 1000 strings which is a datasource for long list
+List<String> getListElements() {
+  var items = List<String>.generate(1000, (counter) => "Item $counter");
+  return items;
+}
+
+// function to wrap datatsource content in widget to show in list view
 Widget getListView() {
+  var listItems = getListElements();
+
+  // syntax og long list
+  //memory efficient because load only those items in memory which are available on screen
+  var listView = ListView.builder(
+      // create 1000 List Tile for long list
+      itemBuilder: (context, index) {
+    //returning 1000 listitems
+    return ListTile(
+        leading: Icon(Icons.arrow_right),
+        title: Text(listItems[index]),
+        onTap: () {
+          debugPrint("${listItems[index]} was tapped");
+        }); // ListTile
+  }); //ListView.builder
+  return listView;
+}
+
+/*Widget getListView() {
   //ListView used for small number of items
   // Listview loads all items in memory so not memory efficient
   var listView = ListView(
@@ -42,4 +68,4 @@ Widget getListView() {
     ], //Widget
   ); //ListView
   return listView;
-}
+}*/
