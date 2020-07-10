a3c04aa2531cda219cea7ed57411d04e3eea45f7
Radhika <radhika23137@gmail.com>
2020-07-10 16:06:39 +0530

    Floating action button, tooltip and snack bar

--- FlutterHandsOn/explore_more_widgets/lib/main.dart
+++ FlutterHandsOn/explore_more_widgets/lib/main.dart
@@ -4,12 +4,33 @@ void main() {
   runApp(MaterialApp(
     title: "Exploring ListView",
     home: Scaffold(
-        appBar: AppBar(title: Text("Long List")),
-        body: getListView() // called it in scaffold widget because
-        // the scrollable content can overflow screen
-        // so would through exception on direct calling so wrapped inside scaffold widget
-        ),
-  ));
+      appBar: AppBar(title: Text("Long List")),
+      body: getListView(), // called it in scaffold widget because
+      // the scrollable content can overflow screen
+      // so would through exception on direct calling so wrapped inside scaffold widget
+      floatingActionButton: FloatingActionButton(
+          // create floating button in bottom right
+          onPressed: () {
+            debugPrint("Floating button tapped");
+          },
+          child: Icon(Icons.add), //show icon of plus in bottom right
+          tooltip: 'Add one more Item'), // FLoating Action Button
+    ), // Scaffold
+  )); //Material App
+}
+
+// function to show snackbar which will called on click of list item
+void showSnackBar(BuildContext context, String item) {
+  var snackBar = SnackBar(
+      content: Text("You just tapped $item"),
+      action: SnackBarAction( //action to be performed on a snack bar like undo, ok 
+        label: "Undo",
+        onPressed: () {
+          debugPrint("Performing Undo operation");
+        },
+      )// SNackBarACtion
+      );// SnackBAr
+  Scaffold.of(context).showSnackBar(snackBar);
 }
 
 // function to generate 1000 strings which is a datasource for long list
@@ -27,12 +48,12 @@ Widget getListView() {
   var listView = ListView.builder(
       // create 1000 List Tile for long list
       itemBuilder: (context, index) {
-    //returning 1000 listitems
+    //returning 1000 list items
     return ListTile(
         leading: Icon(Icons.arrow_right),
         title: Text(listItems[index]),
         onTap: () {
-          debugPrint("${listItems[index]} was tapped");
+          showSnackBar(context, listItems[index]);
         }); // ListTile
   }); //ListView.builder
   return listView;
