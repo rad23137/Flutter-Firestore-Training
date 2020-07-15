469c8665355213a8de1dd742b5afdf35370d29b9
Radhika <radhika23137@gmail.com>
2020-07-15 19:35:07 +0530

    More styling by combining columns, rows and container

--- UdemyCourse/expense_planner/lib/main.dart
+++ UdemyCourse/expense_planner/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expense_planner/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,35 +6,78 @@ void main() {
 }
 
 class MyHome extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't2',
+        title: 'Weekely Groceries',
+        amount: 16.53,
+        date: DateTime.now())
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Fultter App"),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly, // position element from top to bottom in columns vice versa in rows
-          crossAxisAlignment: CrossAxisAlignment.stretch, // position element from left to right in columns vice versa in rows
-          children:<Widget>[
-          Container(
-          width:double.infinity,
-          child:Card(
-          color: Colors.blue,
-          child:Text("Chart!"),
-          )
+          appBar: AppBar(
+            title: Text("Fultter App"),
           ),
-          Container(
-          child:Card(  // Card take width of child but to change that either wrap card in container or child in container
-            color: Colors.red,
-          child:Text("List of Transactions!"),
-          ),
-          )
-
-          ]
-         
-        )),
-      );
+          body: Column(
+              mainAxisAlignment: MainAxisAlignment
+                  .spaceEvenly, // position element from top to bottom in columns vice versa in rows
+              crossAxisAlignment: CrossAxisAlignment
+                  .stretch, // position element from left to right in columns vice versa in rows
+              children: <Widget>[
+                Container(
+                    width: double.infinity,
+                    child: Card(
+                      color: Colors.blue,
+                      child: Text("Chart!"),
+                    )),
+                Column(
+                  children: transactions.map((tx) {
+                    return Card(
+                        child: Row(
+                      children: <Widget>[
+                        Container(
+                            margin: EdgeInsets.symmetric(
+                              vertical: 10,
+                              horizontal: 15,
+                            ),
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                              color: Colors.purple,
+                              width: 2,
+                            )),
+                            padding: EdgeInsets.all(10),
+                            child: Text(
+                              "\$ ${tx.amount}",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.purple,
+                              ),
+                            )),
+                        Container(
+                            child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(tx.title,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                )),
+                            Text(tx.date.toString(),
+                                style: TextStyle(
+                                    fontSize: 16, color: Colors.grey)),
+                          ],
+                        ))
+                      ],
+                    ));
+                  }).toList(),
+                )
+              ])),
+    );
   }
 }
