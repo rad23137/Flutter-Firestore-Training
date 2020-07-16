1b03284d23c1283c09b03565fe27ead44d47123c
Radhika <radhika23137@gmail.com>
2020-07-16 12:24:27 +0530

    Seperation of widgets and create stateful dynamic transactions list

--- UdemyCourse/expense_planner/lib/main.dart
+++ UdemyCourse/expense_planner/lib/main.dart
@@ -1,26 +1,18 @@
-import 'package:expense_planner/transaction.dart';
+
+import 'package:expense_planner/widgets/user_transactions.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+
 
 void main() {
   runApp(MyHome());
 }
 
 class MyHome extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
-    Transaction(
-        id: 't2',
-        title: 'Weekely Groceries',
-        amount: 16.53,
-        date: DateTime.now())
-  ];
+ 
   //String titleInput;
   //String amountInput;
 
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
@@ -41,81 +33,8 @@ class MyHome extends StatelessWidget {
                       color: Colors.blue,
                       child: Text("Chart!"),
                     )),
-                Card(
-                    elevation: 5,
-                    child: Container(
-                        padding: EdgeInsets.all(10),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: <Widget>[
-                            TextField(
-                              decoration: InputDecoration(labelText: 'Title'),
-                              controller: titleController,
-                              // onChanged: (val) {
-                              //   titleInput =
-                              //       val; //storing user input in a property
-                              // },
-                            ),
-                            TextField(
-                              decoration: InputDecoration(labelText: 'Amount'),
-                              controller: amountController,
-                              // onChanged: (val) {
-                              //   amountInput =
-                              //       val; // storing user input in a property
-                              // },
-                            ),
-                            FlatButton(
-                              child: Text("Add Transaction"),
-                              onPressed: () {
-                                print(titleController.text);
-                                print(amountController.text);
-                              },
-                              textColor: Colors.purple,
-                            )
-                          ],
-                        ))),
-                Column(
-                  children: transactions.map((tx) {
-                    return Card(
-                        child: Row(
-                      children: <Widget>[
-                        Container(
-                            margin: EdgeInsets.symmetric(
-                              vertical: 10,
-                              horizontal: 15,
-                            ),
-                            decoration: BoxDecoration(
-                                border: Border.all(
-                              color: Colors.purple,
-                              width: 2,
-                            )),
-                            padding: EdgeInsets.all(10),
-                            child: Text(
-                              "\$${tx.amount}",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: Colors.purple,
-                              ),
-                            )),
-                        Container(
-                            child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(tx.title,
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18,
-                                )),
-                            Text(DateFormat('yyyy/MMM/dd').format(tx.date),
-                                style: TextStyle(
-                                    fontSize: 16, color: Colors.grey)),
-                          ],
-                        ))
-                      ],
-                    ));
-                  }).toList(),
-                )
+               
+                UserTransactions(), //Rendering Lists of Transaction
               ])),
     );
   }
---
+++ UdemyCourse/expense_planner/lib/widgets/user_transactions.dart
@@ -0,0 +1,41 @@
+import 'package:expense_planner/widgets/new_transaction.dart';
+import 'package:expense_planner/widgets/transaction_list.dart';
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+
+class UserTransactions extends StatefulWidget {
+  @override
+  _UserTransactionsState createState() => _UserTransactionsState();
+}
+
+class _UserTransactionsState extends State<UserTransactions> {
+  final List<Transaction> _userTransaction = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+      id: 't2',
+      title: 'Weekely Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    )
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+        title: txTitle,
+        amount: txAmount,
+        date: DateTime.now(),
+        id: DateTime.now().toString());
+
+    setState(() {
+      _userTransaction.add(newTx);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[NewTransaction(_addNewTransaction), TransactionList(_userTransaction)],
+    );
+  }
+}
