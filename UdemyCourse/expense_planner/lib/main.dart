c189c110c64045a46b072b062ac056b839107808
Radhika <radhika23137@gmail.com>
2020-07-16 15:55:02 +0530

    Adding user input as ModelBootmSheet on clcik of button and close it on adding values

--- UdemyCourse/expense_planner/lib/main.dart
+++ UdemyCourse/expense_planner/lib/main.dart
@@ -1,52 +1,90 @@
-
-import 'package:expense_planner/widgets/user_transactions.dart';
+import 'package:expense_planner/widgets/new_transaction.dart';
 import 'package:flutter/material.dart';
-
+import './models/transaction.dart';
+import 'widgets/transaction_list.dart';
 
 void main() {
-  runApp(MyHome());
+  runApp(MaterialApp(
+    title: "Flutter App",
+    home: MyHome()));
 }
 
-class MyHome extends StatelessWidget {
- 
+class MyHome extends StatefulWidget {
   //String titleInput;
   //String amountInput;
 
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
 
+class _MyHomeState extends State<MyHome> {
+  final List<Transaction> _userTransaction = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+      id: 't2',
+      title: 'Weekely Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    )
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+        title: txTitle,
+        amount: txAmount,
+        date: DateTime.now(),
+        id: DateTime.now().toString());
+
+    setState(() {
+      _userTransaction.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            onTap: () {},
+            child: NewTransaction(_addNewTransaction),
+            behavior: HitTestBehavior.opaque,
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text("Fultter App"),
-            actions: <Widget>[
-              IconButton(icon: Icon(Icons.add), onPressed: (){})
-            ],
-          ),
-          body: SingleChildScrollView( //Make the wholde content under body scrollable
-          child:Column(
-              //mainAxisAlignment: MainAxisAlignment  .spaceEvenly, // position element from top to bottom in columns vice versa in rows
-              crossAxisAlignment: CrossAxisAlignment
-                  .stretch, // position element from left to right in columns vice versa in rows
-              children: <Widget>[
-                Container(
-                    width: double.infinity,
-                    child: Card(
-                      color: Colors.blue,
-                      child: Text("Chart!"),
-                    )),
-               
-                UserTransactions(), //Rendering Lists of Transaction
-              ]
-              )
-              ),
-              floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-              floatingActionButton:FloatingActionButton(child: Icon(Icons.add),
-              onPressed: (){}) ,
-
-              ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Fultter App"),
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () => _startAddNewTransaction(context))
+          ],
+        ),
+        body: SingleChildScrollView(
+            //Make the wholde content under body scrollable
+            child: Column(
+                //mainAxisAlignment: MainAxisAlignment  .spaceEvenly, // position element from top to bottom in columns vice versa in rows
+                crossAxisAlignment: CrossAxisAlignment
+                    .stretch, // position element from left to right in columns vice versa in rows
+                children: <Widget>[
+              Container(
+                  width: double.infinity,
+                  child: Card(
+                    color: Colors.blue,
+                    child: Text("Chart!"),
+                  )),
+              TransactionList(
+                  _userTransaction), //Rendering Lists of Transaction
+            ])),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () => _startAddNewTransaction(context)),
+      );
+    
   }
 }
--- UdemyCourse/expense_planner/lib/widgets/new_transaction.dart
+++ UdemyCourse/expense_planner/lib/widgets/new_transaction.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+class NewTransaction extends StatefulWidget {
   final Function addTx;
 
   NewTransaction(this.addTx);
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final titleController = TextEditingController();
+
+  final amountController = TextEditingController();
 
   void submitData() {
     final enteredTitle = titleController.text;
@@ -14,7 +21,11 @@ class NewTransaction extends StatelessWidget {
     if (enteredTitle.isEmpty || enteredAmount <= 0) {
       return;
     }
-    addTx(enteredTitle, enteredAmount);
+    widget.addTx(enteredTitle,
+        enteredAmount); //widget.addTx so that the method in NewTransaction Class can also be used here
+
+    Navigator.of(context).pop(); // To close the model bottom sheet on submit
+    
   }
 
   @override
