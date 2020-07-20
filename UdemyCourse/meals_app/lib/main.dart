32b077cc0aca1d44bb0f8037a674132791bc746a
Radhika <radhika23137@gmail.com>
2020-07-20 20:00:51 +0530

    Display text on image and show some details using enum

--- UdemyCourse/meals_app/lib/main.dart
+++ UdemyCourse/meals_app/lib/main.dart
@@ -1,8 +1,8 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:meals_app/categories_screen.dart';
-import 'package:meals_app/category_meals_screen.dart';
+import 'package:meals_app/screens/categories_screen.dart';
+import 'package:meals_app/screens/category_meals_screen.dart';
 
 void main() {
   runApp(MyApp());
---
+++ UdemyCourse/meals_app/lib/screens/category_meals_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/widgets/meal_item.dart';
+
+import '../dummy_data.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
+
+  static const routeName =
+      '/category-meals'; // store the route name to this screen
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs = ModalRoute.of(context).settings.arguments as Map<String,
+        String>; // Extraction of Route Arguments using ModalRoute
+
+    final categoryId =
+        routeArgs['id']; // saving the argument value of id in variable
+    final categoryTitle =
+        routeArgs['title']; // Saving the argument value of title in variable
+
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
+    return Scaffold(
+        appBar: AppBar(title: Text(categoryTitle)),
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            return MealItem(title: categoryMeals[index].title, imageUrl: categoryMeals[index].imageUrl, duration: categoryMeals[index].duration, complexity: categoryMeals[index].complexity, affordability: categoryMeals[index].affordability);
+          },
+          itemCount: categoryMeals.length,
+        ));
+  }
+}
