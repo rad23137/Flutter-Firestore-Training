b8ce5c42454b5a8e186df56d88a331ca93d38bee
Radhika <radhika23137@gmail.com>
2020-07-21 13:26:25 +0530

    Used PushedReplacementNamed Route to avoid stacking

--- UdemyCourse/meals_app/lib/screens/filters_screen.dart
+++ UdemyCourse/meals_app/lib/screens/filters_screen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
 
 class FiltersScreen extends StatelessWidget {
   static const routeName = '/filters';
   @override
   Widget build(BuildContext context) {
-    return Container( child: Text("Filters"));
+    return Scaffold(
+      appBar: AppBar(title: Text("Your Filters"),),
+      drawer: MainDrawer(),
+      body:Container( child: Text("Filters"),)
+    );
   }
 }
