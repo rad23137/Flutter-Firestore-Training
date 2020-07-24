c085c0005d07d6ba1fec548a4f506e58cb30b85b
Radhika <radhika23137@gmail.com>
2020-07-24 13:58:17 +0530

    Manage Products Screen and user input using Form

--- UdemyCourse/shop_app/lib/main.dart
+++ UdemyCourse/shop_app/lib/main.dart
@@ -3,10 +3,12 @@ import 'package:shop_app/providers/cart.dart';
 import 'package:shop_app/providers/orders.dart';
 import 'package:shop_app/providers/products.dart';
 import 'package:shop_app/screens/cart_screen.dart';
+import 'package:shop_app/screens/edit_product_screen.dart';
 import 'package:shop_app/screens/orders_screen.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/products_overview_screen.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/screens/user_products_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -41,6 +43,8 @@ class MyApp extends StatelessWidget {
             ProductDetailScreen.routeName: (context) => ProductDetailScreen(),
             CartScreen.routeName:(context)=>CartScreen(),
             OrderScreen.routeName: (context)=> OrderScreen(),
+            UserProductsScreen.routeName: (context)=> UserProductsScreen(),
+            EditProductScreen.routeName: (context)=> EditProductScreen(),
           },
     )
     );
