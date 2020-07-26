ef2e8b4432f3e64ce2994a972cb1434130be16e9
Radhika <radhika23137@gmail.com>
2020-07-26 20:37:41 +0530

    Completed get request and pll to refresh

--- UdemyCourse/shop_app/lib/providers/products.dart
+++ UdemyCourse/shop_app/lib/providers/products.dart
@@ -1,11 +1,10 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:shop_app/dummy_products.dart';
 import 'package:shop_app/providers/product.dart';
 
 class Products with ChangeNotifier {
-  List<Product> _items = DUMMY_PRODUCTS;
+  List<Product> _items = [];
 
   // var _showFavoritesOnly = false;
 
@@ -32,36 +31,58 @@ class Products with ChangeNotifier {
     return _items.firstWhere((prod) => prod.id == id);
   }
 
-  Future<void> addProduct(Product product) async{
+  Future<void> fetchAndSetProducts() async {
+    const url = 'https://flutter-project-f422c.firebaseio.com/products.json';
+
+    try {
+      final response = await http.get(url);
+
+      final extractedData = json.decode(response.body) as Map<String, dynamic>;
+      final List<Product> loadedProducts = [];
+      extractedData.forEach((prodId, prodData) {
+        loadedProducts.add(Product(
+            id: prodId,
+            title: prodData['title'],
+            price: prodData['price'],
+            description: prodData['description'],
+            imageUrl: prodData['imageUrl'],
+            isFavorite: prodData['isFavorite']));
+      });
+      _items = loadedProducts;
+      notifyListeners();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  Future<void> addProduct(Product product) async {
     //_items.add(value);
 
-    const url = 'https://flutter-project-f422c.firebaseio.com/products.json'; // url of database
-try{
-   final response= await http.post(url, body: json.encode({   // http post request to add a product
-      'title': product.title,
-      'description': product.description,
-      'price': product.price,
-      'imageUrl': product.imageUrl,
-      'isFavorite': product.isFavorite
-    }));
+    const url =
+        'https://flutter-project-f422c.firebaseio.com/products.json'; // url of database
+    try {
+      final response = await http.post(url,
+          body: json.encode({
+            // http post request to add a product
+            'title': product.title,
+            'description': product.description,
+            'price': product.price,
+            'imageUrl': product.imageUrl,
+            'isFavorite': product.isFavorite
+          }));
 
-       final newProduct = Product(
-        id: json.decode(response.body)['name'],
-        title: product.title,
-        description: product.description,
-        price: product.price,
-        imageUrl: product.imageUrl)
-        ;
-    _items.add(newProduct);
-    notifyListeners();
-    }
-    catch(error)
-    {
+      final newProduct = Product(
+          id: json.decode(response.body)['name'],
+          title: product.title,
+          description: product.description,
+          price: product.price,
+          imageUrl: product.imageUrl);
+      _items.add(newProduct);
+      notifyListeners();
+    } catch (error) {
       print(error);
       throw error;
     }
-
-   
   }
 
   void updateProduct(String id, Product newProduct) {
